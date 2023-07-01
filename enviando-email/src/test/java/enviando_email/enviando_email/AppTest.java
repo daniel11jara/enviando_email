89b15a97c64b35c37aa613cdde1b259cdd720c18
daniel jara <daniel11jara@gmail.com>
2023-07-01 09:06:52 -0300

    aula 03

--- enviando-email/src/test/java/enviando_email/enviando_email/AppTest.java
+++ enviando-email/src/test/java/enviando_email/enviando_email/AppTest.java
@@ -2,6 +2,8 @@ package enviando_email.enviando_email;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.Properties;
+
 import org.junit.Test;
 
 /**
@@ -11,6 +13,26 @@ public class AppTest
 {
 	@Test
    public void testeEmail() {
-	   
+		
+		//instanciando a classe properties
+		Properties properties = new Properties();
+		
+		//passando a autenticação / autorização
+		properties.put("mail.smtp.auth", "true");
+		
+		//parte de segurança - tls
+		properties.put("mail.smtp.starttls", "true");
+		
+		//servidor gmail do google
+		properties.put("mail.smtp.host", "smtp.gmail.com");
+		
+		//passando a porta do servidor
+		properties.put("mail.smtp.port", "465");
+		
+		//especifica a porta a ser conectada pelo socket
+		properties.put("mail.smtp.socketFactory.port", "465");
+		
+		//classe socket de conexão ao smtp
+		properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
    }
 }
